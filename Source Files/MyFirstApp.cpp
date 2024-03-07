6b3c9e251e4b737ca36480b84f4f82e426bd0239
Lisa <llvvss@icloud.com>
2024-03-07 09:05:20 -0700

    first commit

---
+++ Source Files/MyFirstApp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;    // allows us to write without the std::
+
+
+int main()
+{
+    cout << "Hello World!\n"; // this will output to the console Hello World!
+    cout << "My name is Lisa."<< endl;
+    // system("pause>0");
+    // std::endl    < that in effect does a carriage return
+    cout << "Today is Thursday.";
+}
