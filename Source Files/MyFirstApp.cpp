af498941bc80569c4a6b5ef92044bc5c12ab49cf
Lisa <llvvss@icloud.com>
2024-03-07 09:11:25 -0700

    added a comment

--- Source Files/MyFirstApp.cpp
+++ Source Files/MyFirstApp.cpp
@@ -6,7 +6,7 @@ int main()
 {
     cout << "Hello World!\n"; // this will output to the console Hello World!
     cout << "My name is Lisa."<< endl;
-    // system("pause>0");
+    // system("pause>0");      this line results in an error
     // std::endl    < that in effect does a carriage return
     cout << "Today is Thursday.";
 }
